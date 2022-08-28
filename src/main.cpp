6cacb5192a1ad7941c367d8e45e6ea3b5368f478
Nathan Page <33336363+ndpage@users.noreply.github.com>
2022-08-27 21:45:52 -0400

    echo arguments passed to main() to std out

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "main.hpp"
 
 int main(int argc, char* argv[]){
-  std::cout << argc;
+  using namespace std;
+  cout << "Number of arguments: " << argc << endl;
+  for(int i; i < argc; i++){
+    cout << "Argument " << i << ": " << argv[i] << endl;
+  }
   return 0;
 }
